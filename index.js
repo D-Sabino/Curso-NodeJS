06eb8827e39ee9e1c1175c421e428dca61355cde
Daniel Sabino <dannisabino17@gmail.com>
2023-01-27 06:16:03 -0300

    Manipulação de arquivos
    Criação, exclusão, etc.
    Manipulando arquivos .txt

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+/* //Executando arquivo .html/iniciando servidor
 const http = require('http');
 const fs = require('fs');
 
 const hotsname = "127.0.0.1"
 const port = 3000;
 
+//Cria novo arquivo
+fs.writeFile('danki.txt', 'teste danki code', function(err){
+    if(err) throw err;
+    console.log('O arquivo foi criado com sucesso!');
+})
+
 const server = http.createServer((req, res)=>{
     if (req.url == '/danki'){
+        //Cria novo arquivo ou insere mais conteudo no arquivo ja existente.
+        fs.appendFile('danki.txt', "\nArquivo atualizado", (err)=>{
+            if(err) throw err;
+            console.log('Arquivo salvo com sucesso!');
+        })
+
+
         fs.readFile('index.html', function(err,data){
             res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data);
@@ -22,3 +36,35 @@ const server = http.createServer((req, res)=>{
 server.listen(port,hotsname,()=>{
     console.log("O servidor esta rodando!");
 })
+*/
+
+/*
+//Fazendo a leitura de um arquivo .txt
+const fs = require('fs');
+const read = require('readline');
+
+fs.readFile('danki.txt', function(err, data){
+    //console.log(data.toString()); //Exibe o conteudo do arquivo no cmd
+    let str = data.toString();
+    //let newStr = str.split('/'); //Organizou o arquivo como um array, o arquivo constava nomes separados por '/'
+    //let newStr = str.substr(1,3); //Informa a partir de qual coluna vai retornar e quantos caracteres vai mostrar
+    
+    console.log(newStr);
+
+})
+*/
+
+
+const fs = require('fs');
+const read = require('readline');
+
+//Função para deletar arquivos
+fs.unlink('danki.txt', function(err){
+    console.log('O arquivo foi deletado.');
+
+})
+
+//Função para renomear arquivos
+fs.rename('danki.txt', 'dankiCode.txt', function (err) { 
+    console.log('Arquivo renomeado');
+ })
