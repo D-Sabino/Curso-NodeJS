8a2d674a913fded2363b134220923bb72f9b6425
Daniel Sabino <dannisabino17@gmail.com>
2023-01-30 05:49:14 -0300

    Lendo inf. do CMD
    Operações simples, escrevendo e captando informações do CMD.

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ fs.readFile('danki.txt', function(err, data){
     console.log(newStr);
 
 })
-*/
+
 
 
 const fs = require('fs');
@@ -67,4 +67,25 @@ fs.unlink('danki.txt', function(err){
 //Função para renomear arquivos
 fs.rename('danki.txt', 'dankiCode.txt', function (err) { 
     console.log('Arquivo renomeado');
+ })
+ */
+
+ //Lendo informacoes do CMD
+ const readline = require('readline');
+
+ const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+ })
+
+ rl.question('Qual o seu nome? ', function(name){
+    console.log('O nome do usuario e: '+name);
+    rl.question('Qual a sua idade? ', function(idade){
+        console.log('A idade e: '+idade);
+    })
+ })
+
+ rl.on('close',function(){
+    console.log('Adeus!');
+    process.exit(0);
  })
