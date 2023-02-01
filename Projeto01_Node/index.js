2d4416738c21478824c5e5b59143b696c62ac7c1
Daniel Sabino <dannisabino17@gmail.com>
2023-02-01 10:18:17 -0300

    Projeto 01
    Apresentando um array e excluindo itens do sistema, projeto básico, sem banco de dados

--- Projeto01_Node/index.js
+++ Projeto01_Node/index.js
@@ -8,9 +8,30 @@ app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/views'));
 
+var tarefas = ['TESTE 01', 'TESTE 02', 'TESTE 03'];
+
 app.get('/',(req,res)=>{
-    res.render('index', {nome: 'Daniel'});
+    //res.render('index', {nome: 'Daniel'});
+    /*
+        Chamada do nome no HTML:
+        <html>
+            <body>
+                <h2>Ola <%= nome %>!</h2>
+            </body>
+        </html>
+    */
+    res.render('index', {tarefaslist:tarefas})
 });
+
+app.get('/deletar/:id', (req, res)=>{
+    console.log(req.params.id);
+    tarefas = tarefas.filter(function(val,index){
+        if (index!= req.params.id){
+            return val;
+        }
+    })
+    res.render('index', {tarefaslist:tarefas})
+})
 
 app.listen(5000,()=>{
     console.log('Server rodando!');
