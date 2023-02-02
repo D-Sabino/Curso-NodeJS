f6dc5112a6c3f8c58a08ae6d63d8df77060dcb5c
Daniel Sabino <dannisabino17@gmail.com>
2023-02-02 18:42:42 -0300

    Finalizando o primeiro projeto

--- Projeto01_Node/index.js
+++ Projeto01_Node/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+var bodyParser = require('body-parser')
+
 const path = require('path');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -9,6 +15,11 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/views'));
 
 var tarefas = ['TESTE 01', 'TESTE 02', 'TESTE 03'];
+
+app.post('/',(req,res)=>{
+    tarefas.push(req.body.tarefa);
+    res.render('index',{tarefaslist:tarefas});
+})
 
 app.get('/',(req,res)=>{
     //res.render('index', {nome: 'Daniel'});
