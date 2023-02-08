4c18f0b187008bee718f5733903e9c1512e8c2ac
Daniel Sabino <dannisabino17@gmail.com>
2023-02-08 07:19:20 -0300

    Finalizando front-end do portal (noticia single)

--- portal_noticias_estatico/index.js
+++ portal_noticias_estatico/index.js
@@ -4,33 +4,37 @@ var bodyParser = require('body-parser')
 const path = require('path');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/pages'));
 
-app.get('/',(req,res)=>{
-    console.log(req.query);
 
+app.get('/',(req,res)=>{
+    
     if(req.query.busca == null){
-        res.render('home',{})
+        res.render('home',{});
     }else{
-        res.send('Você buscou: '+req.query.busca);
+        res.render('busca',{});
     }
 
-})
+  
+});
+
 
 app.get('/:slug',(req,res)=>{
-    res.send(req.params.slug);
+    //res.send(req.params.slug);
+    res.render('single',{});
 })
 
 
 
 app.listen(5000,()=>{
-    console.log('Server rodando!');
+    console.log('server rodando!');
 })
